ae87cb533565c0573ff2fd4b44482406118a6f9f
Jon <jonathan.woolfrey@gmail.com>
2025-04-01 14:27:05 +0100

    New file

---
+++ launch/follow_twist.py
@@ -0,0 +1,57 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    this_directory = get_package_share_directory('kuka_velocity_control')
+
+    # Node: Trajectory Tracking Server
+    trajectory_tracking = Node(
+        package    = 'serial_link_action_server',
+        executable = 'follow_twist_server',
+        name       = 'follow_twist_server',
+        output     = 'screen',
+        parameters = [os.path.join(this_directory, 'config/control_parameters.yaml')],
+        arguments  = [os.path.join(this_directory, 'urdf/iiwa14.urdf'),                             # URDF location
+                     'link7',                                                                       # Endpoint name
+                     'joint_command_relay',                                                         # Topic to publish joint commands to
+                     'joint_state']                                                                 # Joint state topic to subscribe to
+    )
+
+    # Joint command relay
+    relay = Node(
+        package    = 'kuka_velocity_control',
+        executable = 'joint_command_relay',
+        name       = 'joint_command_relay',
+        output     = 'screen',
+        arguments  = ['joint_command_relay',                                                        # Node name
+                      'joint_command_relay',                                                        # Subscribe topic
+                      'joint_commands']                                                             # Publish topic
+    )
+    
+    # Joystick node
+    joy = Node(
+        package    = 'joy',
+        executable = 'joy_node',
+        name       = 'joy_node',
+        parameters = [{"device_id": 0},               # Use first joystick
+                      {"deadzone": 0.1},              # Ignore small movements under 10%
+                      {"autorepeat_rate": 20.0},      # Publish at 20 Hz even if no new input
+                      {"coalesce_interval_ms": 5},    # Group events for 5ms before publishing
+                      {"sticky_buttons": False}       # Normal button behavior (no sticky mode)
+        ]
+    )
+        
+    # Joy->Twist Mapper
+    mapper = Node(
+        package    = 'serial_link_interfaces',
+        executable = 'joy_twist_mapper',
+        name       = 'joy_twist_mapper',
+        output     = 'screen',
+        parameters = [os.path.join(this_directory, 'config/wii_nunchuck.yaml')]
+    )
+
+    return LaunchDescription([trajectory_tracking, relay, joy, mapper])
+
